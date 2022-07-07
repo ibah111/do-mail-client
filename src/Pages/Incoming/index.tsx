538dacd6adf0a0ea92e31b7c8b5e64fb09cad89f
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-07 16:49:56 +0300

    Добавил Paper

--- src/Pages/Incoming/index.tsx
+++ src/Pages/Incoming/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import getGrid from "../../api/getGrid";
 import useGrid from "./Hooks/useGrid";
 import DataGrid from "./DataGrid";
+import { Paper } from "@mui/material";
 export default function Incoming() {
   const { state, setMail, typData } = useGrid();
   React.useEffect(() => {
@@ -11,7 +12,9 @@ export default function Incoming() {
   }, [typData, state.filterModel, state.page, state.pageSize, state.sortModel]);
   return (
     <>
-      <DataGrid />
+      <Paper>
+        <DataGrid />
+      </Paper>
     </>
   );
 }
