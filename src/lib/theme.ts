1b6fb2c32867c126f8208a23ac11a53f5f80be47
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-07 09:49:27 +0300

    Локализация

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,3 +1,5 @@
 import { createTheme } from "@mui/material";
-const theme = createTheme();
+import { ruRU } from "@mui/material/locale";
+import { ruRU as ruRUGrid } from "@mui/x-data-grid-premium";
+const theme = createTheme({}, ruRU, ruRUGrid);
 export default theme;
