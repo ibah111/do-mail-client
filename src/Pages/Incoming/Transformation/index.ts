0eb880184dd114bceeeedea8badcf70a13ee3dee
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-08 09:51:13 +0300

    Дописал трансформацию

--- src/Pages/Incoming/Transformation/index.ts
+++ src/Pages/Incoming/Transformation/index.ts
@@ -1,15 +1,26 @@
 import { ClassConstructor } from "class-transformer";
 import {
+  ArhiveIncomingCourtBailiffMailState,
+  ArhiveIncomingCourtMailState,
+  ArhiveIncomingGovernmentMailState,
+  ArhiveIncomingMailState,
+  DataIncomingState,
+  IncomingCourtBailiffMailState,
+  IncomingCourtMailState,
   IncomingGovernmentMailState,
   IncomingMailState,
 } from "../../../Types/dataIncoming";
-interface TransformationState {
-  [index: string]: ClassConstructor<any>;
-  IncomingMail: ClassConstructor<IncomingMailState>;
-  IncomingGovernmentMail: ClassConstructor<IncomingGovernmentMailState>;
-}
+type TransformationState = {
+  [index in keyof DataIncomingState]: ClassConstructor<any>;
+};
 const Transformation: TransformationState = {
   IncomingMail: IncomingMailState,
   IncomingGovernmentMail: IncomingGovernmentMailState,
+  IncomingCourtMail: IncomingCourtMailState,
+  IncomingCourtBailiffMail: IncomingCourtBailiffMailState,
+  ArhiveIncomingMail: ArhiveIncomingMailState,
+  ArhiveIncomingGovernmentMail: ArhiveIncomingGovernmentMailState,
+  ArhiveIncomingCourtMail: ArhiveIncomingCourtMailState,
+  ArhiveIncomingCourtBailiffMail: ArhiveIncomingCourtBailiffMailState,
 };
 export default Transformation;
