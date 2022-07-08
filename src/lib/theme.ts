ce3912e6dc9c781db8506d0cbeea7fc4e1170ef3
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-08 10:08:17 +0300

    Добавил шрифты

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,5 +1,15 @@
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 import { ruRU } from "@mui/material/locale";
 import { ruRU as ruRUGrid } from "@mui/x-data-grid-premium";
-const theme = createTheme({}, ruRU, ruRUGrid);
+const theme = responsiveFontSizes(
+  createTheme(
+    {
+      typography: {
+        fontFamily: "Roboto",
+      },
+    },
+    ruRU,
+    ruRUGrid
+  )
+);
 export default theme;
