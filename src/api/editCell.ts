d24402f96a0a84f0f8e961ce86563318c9a1e58b
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-12 17:50:24 +0300

    Сделал чтобы была строка

--- src/api/editCell.ts
+++ src/api/editCell.ts
@@ -10,7 +10,7 @@ export default async function editCell(
   name: string,
   data: any
 ): Promise<null> {
-  let value: string = data;
+  let value: string = String(data);
   if (data instanceof Date) {
     value = moment(data).toISOString();
   }
