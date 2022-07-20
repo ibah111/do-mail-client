71d41c32b2e6c2444b58cf10408ef9b959204b7d
Alex Dev Sm <leshasmlesha@gmail.com>
2022-07-20 14:51:46 +0300

    Добавил шрифты

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { createTheme, responsiveFontSizes } from '@mui/material';
 import { ruRU } from '@mui/material/locale';
 import { ruRU as ruRUGrid } from '@mui/x-data-grid-premium';
