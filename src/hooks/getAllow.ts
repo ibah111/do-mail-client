8d6d6be209eccd4b5650461d85b3b7733424a1a8
Alex Dev Sm <leshasmlesha@gmail.com>
2023-03-13 11:31:06 +0300

    Убрал лог

--- src/hooks/getAllow.ts
+++ src/hooks/getAllow.ts
@@ -2,7 +2,6 @@ import { store } from '../Reducer';
 export type AllowFunction = (...userRoles: string[]) => boolean;
 const getAllow = (): AllowFunction => {
   const roles = store.getState().User.roles;
-  console.log(roles);
   return (...userRoles: string[]) => {
     let result = 0;
     for (const role of userRoles) {
