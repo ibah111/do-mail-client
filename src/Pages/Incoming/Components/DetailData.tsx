050d500bceb8741de1e5dcb71b475e776ec0330e
Ibah <starlord77@mail.ru>
2024-02-14 12:38:22 +0300

    feat(details-columns): добавление колонок во вложения

--- src/Pages/Incoming/Components/DetailData.tsx
+++ src/Pages/Incoming/Components/DetailData.tsx
@@ -1,29 +1,76 @@
 import { DataGridPremium, GridColDef } from '@mui/x-data-grid-premium';
 import { Doc } from '../../../Schemas/Doc';
-const columns: GridColDef<Doc>[] = [
-  { field: 'id', type: 'number', headerName: 'ID' },
-  { field: 'doc_id', type: 'number', headerName: 'ID документа в контакте' },
-  {
-    field: 'name',
-    type: 'string',
-    headerName: 'Название документа',
-    width: 300,
-    valueGetter: (params) => params.row.DocAttach?.name,
-  },
-  {
-    type: 'string',
-    field: 'barcode',
-    width: 300,
-    headerName: 'Штрихкод',
-    description: 'Данное поле указывает номер штрихкода в ScannerDocs',
-    valueGetter(params) {
-      return params.row.barcode;
+import { IconButton, Tooltip } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import createCode from '../../../api/ScannerDocsApi/createCode';
+
+class NeedForApi {
+  mail_id: number;
+  law_id: number;
+}
+
+function docColumns({ mail_id, law_id }: NeedForApi): GridColDef<Doc>[] {
+  const columns: GridColDef<Doc>[] = [
+    { field: 'id', type: 'number', headerName: 'ID' },
+    { field: 'doc_id', type: 'number', headerName: 'ID документа в контакте' },
+    {
+      field: 'name',
+      type: 'string',
+      headerName: 'Название документа',
+      width: 300,
+      valueGetter: (params) => params.row.DocAttach?.name,
     },
-  },
-];
+    {
+      type: 'string',
+      field: 'barcode',
+      width: 300,
+      headerName: 'Штрихкод',
+      description: 'Данное поле указывает номер штрихкода в ScannerDocs',
+      valueGetter(params) {
+        const barcode = params.row.barcode;
+        return barcode ? barcode : 'Штрихкод не присвоен';
+      },
+    },
+    {
+      type: 'actions',
+      field: 'actions',
+      width: 150,
+      getActions: (params) => [
+        <>
+          {!params.row.barcode ? (
+            <Tooltip title={'Присвоить штрихкод'}>
+              <IconButton
+                onClick={() =>
+                  createCode({
+                    contact_doc_id: params.row.doc_id,
+                    title: params.row.DocAttach!.name,
+                    law_act_id: law_id,
+                    mail_id,
+                    doc_type: 2,
+                  })
+                }
+              >
+                <AddIcon />
+              </IconButton>
+            </Tooltip>
+          ) : (
+            'Баркод уже присовен'
+          )}
+        </>,
+      ],
+    },
+  ];
+  return columns;
+}
+
 interface DetailDataProps {
   docs: Doc[];
 }
-export default function DetailData({ docs }: DetailDataProps) {
-  return <DataGridPremium autoHeight columns={columns} rows={docs} />;
+export default function DetailData(
+  { docs }: DetailDataProps,
+  api_data: NeedForApi,
+) {
+  return (
+    <DataGridPremium autoHeight columns={docColumns(api_data)} rows={docs} />
+  );
 }
