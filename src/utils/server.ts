4be5f9614eb98d2dbfd8ccb594d3e414cdf9c190
Ibah <starlord77@mail.ru>
2024-05-20 17:22:22 +0300

    fix(url): fixed url server function

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,11 +1,11 @@
 import config from '../config/server.json';
 export default function server(name?: string) {
+  const node = process.env.NODE_ENV;
+  const server = node === 'prod' ? config.server : config.localserver;
   switch (name) {
     case 'oauth':
       return config.oauth;
     default:
-      return process.env.NODE_ENV === 'prod'
-        ? config.server
-        : config.localserver;
+      return server;
   }
 }
