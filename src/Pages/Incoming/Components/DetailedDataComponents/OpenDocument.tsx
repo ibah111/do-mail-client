dc591f1c979680d329a15a2704b9aabbe086a21d
Ibah <starlord77@mail.ru>
2024-08-12 12:09:39 +0300

    feat: dialog file

--- src/Pages/Incoming/Components/DetailedDataComponents/OpenDocument.tsx
+++ src/Pages/Incoming/Components/DetailedDataComponents/OpenDocument.tsx
@@ -1,4 +1,4 @@
-import { Box, Dialog, Grid, Typography } from '@mui/material';
+import { Box, Dialog, Grid, IconButton, Typography } from '@mui/material';
 import { GridActionsCellItem } from '@mui/x-data-grid-premium';
 import React from 'react';
 import PanoramaIcon from '@mui/icons-material/Panorama';
@@ -17,88 +17,35 @@ import { pageNavigationPlugin } from '@react-pdf-viewer/page-navigation';
 import { searchPlugin } from '@react-pdf-viewer/search';
 import getDocuments from '../../../../api/ScannerDocsApi/getDocuments';
 import { changeMime } from '../../../../utils/fileConvert';
+import DialogFile from './DialogFile';
+import Close from '@mui/icons-material/Close';
 
 interface DialogFileProps {
   id: number;
   title: string;
 }
 
-export default function OpenDocuments({ id, title }: DialogFileProps) {
+export default function OpenDocuments({ id }: DialogFileProps) {
   const [open, setOpen] = React.useState(false);
-  const [file, setFile] = React.useState<string>('');
-  React.useEffect(() => {
-    if (open)
-      getDocuments(id).then((res) => {
-        const file = changeMime(title, res);
-        setFile(URL.createObjectURL(file));
-      });
-  }, [open]);
+  const [file, setFile] = React.useState<number | null>(null);
 
-  const defaultLayoutPluginInstance = defaultLayoutPlugin({
-    sidebarTabs: (defaultTabs) => [defaultTabs[0]],
-  });
-  const searchPluginInstance = searchPlugin();
-  const pageNavigationPluginInstance = pageNavigationPlugin();
-  const renderError = (error: LoadError) => {
-    let message = '';
-    switch (error.name) {
-      case 'InvalidPDFException':
-        message = 'Документ недействителен или поврежден';
-        break;
-      case 'MissingPDFException':
-        message = 'Документ отсутствует';
-        break;
-      case 'UnexpectedResponseException':
-        message = 'Неожиданный ответ сервера';
-        break;
-      default:
-        message = 'Не удается загрузить документ';
-        break;
-    }
-    return (
-      <>
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ pt: 10 }}
-        >
-          <Box sx={{ width: 600, height: 40, backgroundColor: '#e53e3e' }}>
-            <Typography sx={{ fontSize: 24 }} align={'center'}>
-              {message}
-            </Typography>
-          </Box>
-        </Grid>
-      </>
-    );
+  const handleOpen = () => {
+    setOpen(true);
+    setFile(id);
   };
+
+  const handleClose = () => {
+    setOpen(false);
+    setFile(null);
+  };
+
+  React.useEffect(() => {}, [open]);
   return (
     <>
-      <Worker workerUrl="public/build/pdf.worker.js">
-        <GridActionsCellItem
-          icon={<PanoramaIcon />}
-          onClick={() => setOpen(true)}
-          label="Открыть документы"
-        />
-        <Dialog open={open} fullScreen onClose={() => setOpen(false)}>
-          <Grid item xs>
-            {file && (
-              <Viewer
-                fileUrl={file}
-                plugins={[
-                  defaultLayoutPluginInstance,
-                  pageNavigationPluginInstance,
-                  searchPluginInstance,
-                ]}
-                defaultScale={1.12}
-                localization={ru_RU}
-                renderError={renderError}
-              />
-            )}
-          </Grid>
-        </Dialog>
-      </Worker>
+      <IconButton onClick={handleOpen}>
+        <PanoramaIcon />
+      </IconButton>
+      {file && <DialogFile open={open} file={file} onClose={handleClose} />}
     </>
   );
 }
