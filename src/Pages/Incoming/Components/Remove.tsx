f1628427f249406da3fab9e67547cfb8966bce12
Ibah <starlord77@mail.ru>
2024-10-31 17:50:57 +0300

    feat(remove): add color to remove button

--- src/Pages/Incoming/Components/Remove.tsx
+++ src/Pages/Incoming/Components/Remove.tsx
@@ -4,7 +4,7 @@ import remove from '../../../api/remove';
 export default function Remove() {
   return (
     <>
-      <Button size="small" onClick={remove}>
+      <Button size="small" onClick={remove} color="error" variant="outlined">
         Удалить
       </Button>
     </>
